acb44d550db173a201297d3a7e872bfa986d3875
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-01 14:37:57 +0300

    CUB-2-Extract connection string in the config, connect app to the db

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,10 @@ const PORT = 5555;
 
 const constants = {
     PORT: 5555,
+
+    URL: "mongodb://127.0.0.1:27017", //Connection string
+
 };
+
 
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 //Imports
 const express = require("express");
+
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
+
 const {PORT} = require('./constants');
 const routes = require('./router');
+
+//Connecting to the database
+dbConnect()
+.then(() => {
+    console.log('Successfully connected to the DB!')
+})
+
+.catch((err) => console.log(`Error while connecting in DB: ${err}`));
 
 //Local variables
 const app = express();
