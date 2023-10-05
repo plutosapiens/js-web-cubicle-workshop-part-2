f90d586163ecdcd96f9fb6ce22bf47fc94875c37
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-05 21:17:24 +0300

    CUB-2-Use accessoryService in cubeController and render in dropdown

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const cubeService = require("../services/cubeService")
+const accessoryService = require('../services/accessoryService');
 
 router.get("/create", (req, res) => {
     res.render("cube/create");
@@ -28,5 +29,15 @@ router.get('/:cubeId/details', async (req, res) => {
 
     res.render('cube/details', { ...cube });
 });
+
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const { cubeId } = req.params;
+    const cube = await cubeService.getSingleCube(cubeId).lean();
+    const accessories = await accessoryService.getAll().lean();
+    const hasAccesories = accessories.length > 0;
+
+    res.render('accessory/attach', { cube, accessories, hasAccesories });
+})
+
 
 module.exports = router;
